9bac7f6ef572906549477e025dbcc801aba69b44
amellinger <36507861+amellinger@users.noreply.github.com>
2023-10-28 23:24:09 -0400

    Add files via upload

---
+++ MakeMosaic.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 28 19:33:22 2023
+
+@author: axel
+"""
+
+from astropy.io import fits
+from astropy.coordinates import SkyCoord
+from reproject.mosaicking import find_optimal_celestial_wcs, reproject_and_coadd
+from reproject import reproject_interp
+from astropy.wcs import WCS
+import matplotlib.pyplot as plt
+
+fnames = ['Sh2-157_F1_PixelMath_astrometry.fits', 'Sh2-157_F2_PixelMath_astrometry.fits']
+weights = ['Sh2-157_F1_PixelMath_astrometry.weight.fits', 'Sh2-157_F2_PixelMath_astrometry.weight.fits']
+
+hdus_3D = [fits.open(fn)[0] for fn in fnames]
+# wcs_out, shape_out = find_optimal_celestial_wcs(hdus, naxis=2)
+
+
+
+for color in range(3):
+    # find_optimal_celestial_wcs only works on 2D images
+    # Therefore we must slice the color fits cube.
+    hdus_2D = []
+    for hdu_3D in hdus_3D:
+        w =  WCS(hdu_3D.header, naxis=2)
+        hdu_2D = w.to_fits()[0]
+        hdu_2D.data = hdu_3D.data[color,:,:]
+        hdus_2D.append(hdu_2D)
+    
+    wcs_out, shape_out = find_optimal_celestial_wcs(hdus_2D)
+    array, footprint = reproject_and_coadd(hdus_2D, wcs_out, shape_out=shape_out,
+                                           reproject_function=reproject_interp,
+                                           input_weights = weights,
+                                           match_background=True)
+
+    hdul_mosaic = wcs_out.to_fits()
+    hdul_mosaic[0].data = array
+    hdul_mosaic.writeto(f'mosaic_{color:01d}.fits', overwrite=True)
+
+# w1 = WCS(hdus[0][0].header, naxis=2)
+# data2D=hdus_2D[0].data
+# plt.imshow(array)
+
+
+# hdul_2D = w1.to_fits()
+# hdul_2D[0].data = data2D
+# hdul_2D.writeto('new.fits', overwrite=True)
